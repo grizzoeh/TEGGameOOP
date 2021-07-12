57fbe6f7745b720e31f673e73e7079fa805ca123
deloquito <45498015+deloquito@users.noreply.github.com>
2021-07-12 10:52:16 -0300

    Mock Combate y Tests catedra listos

---
+++ src/main/test/edu/fiuba/algo3/modelo/CombateMock.java
@@ -0,0 +1,43 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.ArrayList;
+
+public class CombateMock {
+    private Pais paisAtacante;
+    private Pais paisDefensor;
+    private int cantAtacante;
+
+
+    public CombateMock(Pais paisAtacanteIngresado, Pais paisDefensorIngresado, int cantAtacanteIngresada) {
+        this.paisAtacante = paisAtacanteIngresado;
+        this.paisDefensor = paisDefensorIngresado;
+        this.cantAtacante = cantAtacanteIngresada;
+    }
+
+    public void generarCombateAtacanteGanador() {
+
+
+        int perdidosAtacante = 0;
+        int perdidosDefensor = 1;
+
+
+        informarResultados(perdidosAtacante, perdidosDefensor);
+    }
+
+    public void generarCombateDefensorGanador() {
+
+
+        int perdidosAtacante = 1;
+        int perdidosDefensor = 0;
+
+
+        informarResultados(perdidosAtacante, perdidosDefensor);
+    }
+
+    public void informarResultados(int perdidosAtacante, int perdidosDefensor) {
+        this.paisAtacante.eliminarEjercitos(perdidosAtacante);
+        this.paisDefensor.eliminarEjercitos(perdidosDefensor);
+
+        if (!this.paisDefensor.estaOcupado()) this.paisAtacante.invadir(this.paisDefensor);
+    }
+}
--- src/main/test/edu/fiuba/algo3/modelo/TestEntrega1.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestEntrega1.java
@@ -16,4 +16,56 @@ public class TestEntrega1 {
         Teg teg = new Teg(jugadores);
         assertTrue(teg.todosLosPaisesOcupados());
     }
+
+    @Test
+    public void test02CombateDefensorGanador() {
+
+        Ejercito ejercitoCeleste = new Ejercito("Celeste");
+        Ejercito ejercitoVerde = new Ejercito("Verde");
+
+        Pais paisDefensa = new Pais("Argentina");
+        Pais paisAtaque = new Pais("Brasil");
+
+        paisDefensa.asignarEjercito(ejercitoCeleste);
+        paisAtaque.asignarEjercito(ejercitoVerde);
+
+        paisDefensa.agregarEjercito();
+        paisAtaque.agregarEjercito();
+        paisAtaque.agregarEjercito();
+
+        CombateMock combate = new CombateMock(paisAtaque,paisDefensa,1);
+        combate.generarCombateDefensorGanador();
+
+
+        assertEquals(1,paisDefensa.obtenerCantidadEjercitos());
+        assertEquals(1,paisAtaque.obtenerCantidadEjercitos());
+        assertEquals(ejercitoCeleste, paisDefensa.getEjercito());
+
+    }
+
+    @Test
+    public void test03CombateAtacanteGanadorEInvade() {
+
+        Ejercito ejercitoCeleste = new Ejercito("Celeste");
+        Ejercito ejercitoVerde = new Ejercito("Verde");
+
+        Pais paisDefensa = new Pais("Argentina");
+        Pais paisAtaque = new Pais("Brasil");
+
+        paisDefensa.asignarEjercito(ejercitoCeleste);
+        paisAtaque.asignarEjercito(ejercitoVerde);
+
+        paisDefensa.agregarEjercito();
+        paisAtaque.agregarEjercito();
+        paisAtaque.agregarEjercito();
+
+        CombateMock combate = new CombateMock(paisAtaque,paisDefensa,1);
+        combate.generarCombateAtacanteGanador();
+
+
+        assertEquals(1,paisDefensa.obtenerCantidadEjercitos());
+        assertEquals(1,paisAtaque.obtenerCantidadEjercitos());
+        assertEquals(ejercitoVerde, paisDefensa.getEjercito());
+
+    }
 }
