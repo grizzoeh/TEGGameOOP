9fee5be6c28e08d3c84bc21300285087cf2aa4a6
tomdv18 <tdellavecchia@fi.uba.ar>
2021-07-17 20:47:01 -0300

    AgregadasPRUEBAS

--- src/main/test/edu/fiuba/algo3/modelo/TestMazo.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestMazo.java
@@ -84,5 +84,29 @@ public class TestMazo {
 
         assertEquals(tarjetaABuscar.obtenerPais(), paisAux);
     }
+    @Test
+    public void test07TrasRepartirTarjetaSeActualizanCantidadesCorrectamente(){
+        Pais paisAux = new Pais("Qatar");
+        ArrayList<Pais> paises = new ArrayList<>();
+        paises.add(paisAux);
 
+        Mazo mazo = new Mazo(paises);
+
+        Tarjeta eliminada = mazo.repartirTarjeta();
+
+        assertEquals(0, mazo.cantidadDeTarjetasEnElMazo());
+        assertEquals(1, mazo.cantidadDeTarjetasRepartidas());
+    }
+    @Test
+    public void test08PedirleTarjetasAUnMazoVacioDevuelveNull(){
+
+        ArrayList<Pais> paises = new ArrayList<>();
+
+        Mazo mazo = new Mazo(paises);
+
+        Tarjeta eliminada = mazo.repartirTarjeta();
+
+        assertTrue(eliminada == null);
+
+    }
 }
