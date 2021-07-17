6833afe78ddb104b5cb4e0b3bf51447b39d92423
tomdv18 <tdellavecchia@fi.uba.ar>
2021-07-16 21:18:06 -0300

    Cambio que pidio edson

---
+++ src/main/test/edu/fiuba/algo3/modelo/TestEntrega2.java
@@ -0,0 +1,31 @@
+package edu.fiuba.algo3.modelo;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//El codigo esta comentado para poder compilar sin problemas
+public class TestEntrega2 {
+    /*
+    @Test
+    public void test01ActivacionDeTarjetas(){
+
+}
+    @Test
+    public void test02RondaDeDosJugadoresConColocacionDeEjercitos(){
+
+    }
+    @Test
+    public void test03RondaDeTresJugadoresConColocacionDeEjercitos(){
+
+    }
+    @Test
+    public void test04RondaDeDosJugadoresConAtaqueYConquistaDeDosPaises(){
+
+    }
+ */
+}
+
+
