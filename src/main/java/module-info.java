88ee0dcbe1217b4beb71b06b12af77ed4a418997
deloquito <45498015+deloquito@users.noreply.github.com>
2021-07-25 20:46:34 -0300

    lectura de archivos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module edu.fiuba.algo3 {
     requires javafx.controls;
     requires annotations;
+    requires opencsv;
     exports edu.fiuba.algo3;
     exports edu.fiuba.algo3.modelo;
     exports edu.fiuba.algo3.vista;
