ab362e62ba8dd919562dbd7400e9fc06c29b469c
Nico Olano <nicoolano9@gmail.com>
2021-07-26 23:30:27 -0300

    etapa inicial agregada

--- src/main/java/edu/fiuba/algo3/App.java
+++ src/main/java/edu/fiuba/algo3/App.java
@@ -12,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -19,6 +20,8 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import javax.swing.text.Position;
 
 /**
  * JavaFX App
@@ -52,7 +55,7 @@ public class App extends Application {
     }
 
     public Scene crearSceneJugadores(Stage stage){
-        Scene sceneSiguiente = crearSceneEtapaInicial(stage);
+        Scene sceneSiguiente = crearSceneEtapaInicial();
         var label = new Label("Seleccione la cantidad de jugadores:\t");
         label.setFont(new Font("Serif", 18));
 
@@ -93,7 +96,7 @@ public class App extends Application {
         return sceneNueva;
     }
 
-    private HBox crearImagenJuego() {
+    private ImageView crearImagenJuego() {
         Image image = new Image("file:imagenes/mapaTeg.png");
         ImageView imageView = new ImageView();
         imageView.setImage(image);
@@ -102,37 +105,58 @@ public class App extends Application {
         imageView.setFitWidth(900);
         imageView.setPreserveRatio(true);
 
-        HBox caja = new HBox(imageView);
-        return caja;
+
+        return imageView;
     }
 
 
-    public Scene crearSceneEtapaInicial(Stage stage) {
-        HBox imageView = crearImagenJuego();
+    public Scene crearSceneEtapaInicial() {
+        ImageView imageView = crearImagenJuego();
 
         var nombre = new Label("Nombre: ");
         var color = new Label("Color: ");
 
-        //no funciona xd
         Button objetivoButton = new Button();
         objetivoButton.setText("Ver Objetivo");
         objetivoButton.setFont(new Font(12));
-        objetivoButton.setOnAction(new BotonObjetivoEventHandler(stage));
+        objetivoButton.setOnAction(new BotonObjetivoEventHandler());
 
-        var etapa = new Label("Etapa Inicial:\t");
+        var etapa = new Label("Etapa Inicial");
         etapa.setFont(new Font("Serif", 18));
+        var paisesLabel = new Label("Paises Disponibles:");
+        ComboBox paises = new ComboBox();
+        paises.getItems().addAll(
+                "Paises..."
+        );
+        TextField cantFichas = new TextField();
+        cantFichas.setPromptText("Fichas Disponibles: [X]");
+        Button agregarButton = new Button();
+        agregarButton.setText("Agregar");
+        var fichas = new Label("Fichas:");
+        Button avanzarButton = new Button();
+        avanzarButton.setText("Terminar Turno");
+        avanzarButton.setAlignment(Pos.CENTER_RIGHT);
 
+        VBox contSupDer = new VBox(nombre, color, objetivoButton);
+        contSupDer.setSpacing(10);
+        HBox seleccionador = new HBox(paisesLabel,paises,fichas,cantFichas, agregarButton, avanzarButton);
+        seleccionador.setSpacing(10);
+        VBox contInfIzq = new VBox(etapa,seleccionador);
+        contInfIzq.setPadding(new Insets(1));
 
-        VBox conteSupDer = new VBox(nombre, color, objetivoButton);
-        conteSupDer.setAlignment(Pos.TOP_RIGHT);
+        HBox contSuperior = new HBox(imageView,contSupDer);
+        contSuperior.setSpacing(5);
+        HBox contInferior = new HBox(contInfIzq);
 
-        HBox contInfIzq = new HBox(etapa);
-        contInfIzq.setAlignment(Pos.BOTTOM_LEFT);
+        VBox contenedor = new VBox(contSuperior,contInferior);
+        contenedor.setPadding(new Insets(10));
 
-        var sceneNueva = new Scene(new StackPane(conteSupDer,contInfIzq,imageView), 1080, 720);
+        var sceneNueva = new Scene(contenedor, 1080, 720);
 
         return sceneNueva;
     }
+
+
 
 
     public static void main(String[] args) {
--- src/main/java/edu/fiuba/algo3/vistas/BotonObjetivoEventHandler.java
+++ src/main/java/edu/fiuba/algo3/vistas/BotonObjetivoEventHandler.java
@@ -2,42 +2,23 @@ package edu.fiuba.algo3.vistas;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
 
 public class BotonObjetivoEventHandler implements EventHandler<ActionEvent> {
-    private Stage stage;
 
-    public BotonObjetivoEventHandler(Stage stage) {
-        this.stage = stage;
+    public BotonObjetivoEventHandler() {
     }
 
     @Override
     public void handle(ActionEvent actionEvent) {
-        System.out.println("xd");
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Objetivo");
+        alert.setHeaderText("Su objetivo:");
+        alert.setContentText("[Objetivo del Jugador Actual]");
 
-        Label label = new Label("Objetivo:");
-        label.setFont(new Font(18));
-        Label objetivo = new Label("[Objetivo del Jugador Actual]");
-        objetivo.setFont(new Font(14));
-        VBox contenedor = new VBox(label, objetivo);
-
-        var escena = new Scene(contenedor, 270, 180);
-
-        stage.setScene(escena);
-
-
-        return;
+        alert.showAndWait();
     }
 
 
