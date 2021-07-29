3b89fa32726875e64ca79c235fc1de262eedb08a
deloquito <45498015+deloquito@users.noreply.github.com>
2021-07-29 19:59:33 -0300

    tratando de crear clases de escenas

--- src/main/java/edu/fiuba/algo3/modelo/distribuciondepaises/Mapa.java
+++ src/main/java/edu/fiuba/algo3/modelo/distribuciondepaises/Mapa.java
@@ -36,41 +36,8 @@ public class Mapa {
     }
 
     public void crearPaises() throws FileNotFoundException {
-        FileReader fileReader = new FileReader(rutaArchivo);
-        CSVReader csvReader = new CSVReader(fileReader);
-
-        ArrayList<String> listaLector;
-        try {
-            String[] linea;
-            csvReader.readNext();
-            while ((linea = csvReader.readNext()) != null) {
-                Pais paisAux = new Pais(linea[0]);
-                paises.put(linea[0],paisAux);
-                continentes.get(linea[1]).agregarPais(paisAux);
-
-            }
-
-            fileReader = new FileReader(rutaArchivo);
-            csvReader = new CSVReader(fileReader);
-            csvReader.readNext();
-            while ((linea = csvReader.readNext()) != null) {
-                ArrayList<Pais> frontera = new ArrayList<Pais>();
-                Pais paisAux = paises.get(linea[0]);
-                String[] listaFronteras = linea[2].replaceAll("^\"|\"$", "").split(",");
-
-                for (String pais : listaFronteras){
-                    frontera.add(paises.get(pais));
-                }
-
-
-                paisAux.agregarFrontera(frontera);
-
-
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        LectorArchivos lector = new LectorArchivos();
+        lector.leerYParsear(rutaArchivo,paises,continentes);
 
 
     }
